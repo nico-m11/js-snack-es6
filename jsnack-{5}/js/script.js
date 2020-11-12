bae292e312b7d1244638453cd5f6c4bd2b506009
Nicola <nicolamelito.11@gmail.com>
2020-11-12 17:06:01 +0100

    Ex.5 completato

---
+++ jsnack-{5}/js/script.js
@@ -0,0 +1,21 @@
+/*Partendo da un array semplice tipo:
+var myArray = ['Michele', 'Fabio', 'Roberto', 'Giovanni', 'Simone', 'Chiara'];
+chiediamo all’utente con 2 prompt due numeri tra 0 e la lunghezza dell’array
+quindi creiamone uno nuovo con solo i valori che hanno la posizione compresa tra i due numeri inseriti dall’utente
+*/
+//definisco l'array di nomi
+const nomi = ['Ernesto', 'Mery', 'Brigida', 'Luisa', 'Martina', 'Gennaro', 'Ciro'];
+// definisco var max Lunghezza
+const maxLung = nomi.length;
+// chiedo all'utente di inserire due numeri
+let primoInput = prompt(`Inserisci un numero da 0 a ${maxLung}`);
+let secondoInput = prompt(`Inserisci un numero da 0 a ${maxLung}`);
+
+
+const newNome = nomi.filter((element, index) => {
+  return index >= primoInput && index <= secondoInput
+});
+
+console.log(newNome);
+
+document.getElementById('output').innerHTML = `I nomi da te scelti sono ${newNome}`
