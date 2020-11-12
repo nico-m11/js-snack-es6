7bb5e214c68d9970c7277f3ce11a18e40d0c9d14
Nicola <nicolamelito.11@gmail.com>
2020-11-12 15:06:27 +0100

    EX. 2 Updated
    commentato quanto fatto in precedenza e svolto come richiesto.

--- jsnack-{2}/js/script.js
+++ jsnack-{2}/js/script.js
@@ -5,7 +5,7 @@ stampare a schermo la bici con peso minore.*/
 
 // creo un array di oggetti
 
-let bike = [
+const bike = [
   {
     'nome': 'Mountain bike',
     'peso': 18
@@ -33,14 +33,28 @@ let bike = [
 ];
 
 // creo una variabile bici leggere per far si che venga sempre prima
-let lightBike = bike[0];
+//let lightBike = bike[0];
 // creo un ciclo per valutare quella con peso minore
-for(var i = 0; i < bike.length; i++) {
-  if(lightBike.peso > bike[i].peso) {
-    lightBike = bike[i];
-  }
-}
-console.log(lightBike);
+//for(var i = 0; i < bike.length; i++) {
+//  if(lightBike.peso > bike[i].peso) {
+//    lightBike = bike[i];
+//  }
+//}
+//console.log(lightBike);
 console.log(bike)
 // riporto nel document.html
-document.getElementById('outputUno').innerHTML = ` La Bici più leggera è: ${lightBike.nome} ${lightBike.peso}`;
+//document.getElementById('outputUno').innerHTML = ` La Bici più leggera è: ${lightBike.nome} ${lightBike.peso}`;
+
+
+/*jsnack 2 - Updated
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal*/
+
+// utilizzo il DESTRUCTURING come richiesto e commento quanto fatto in precenza
+const [, , , , bikeOne] = bike;
+console.log(bikeOne);
+
+
+// riporto in HTML
+document.getElementById('outputUno').innerHTML = `La bici più leggera è: ${bikeOne.nome} ${bikeOne.peso}`;
